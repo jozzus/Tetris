54e4b20dfb8ac4cee9625dd0ac8f4a4e89ae55f9
Aleksandr Tyutyunnik <aleksandr.tyutyunnik@ingrammicro.com>
2021-12-17 19:09:17 +0100

    Beta version

---
+++ main.py
@@ -0,0 +1,110 @@
+import random
+import sys
+import pygame
+from pygame.locals import *
+from pygame.math import Vector2 as vec
+from shape import Shape, Board
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.font.init()
+    WIDTH, HEIGHT = 400, 850
+    FPS = 5
+    BOARD_SIZE = vec(WIDTH // 25, HEIGHT // 25)
+    FramePerSec = pygame.time.Clock()
+    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Game")
+    random_x = random.randint(0, WIDTH // 25)
+    fig = Shape(25)
+    board = Board(BOARD_SIZE)
+    while not board.can_it_be_placed(fig.get_move_position(vec(random_x, 0))):
+        random_x = random.randint(0, WIDTH // 25)
+    fig.move(vec(random_x, 0))
+
+    gameover = False
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    while True:
+
+        if not gameover:
+            if board.can_it_be_placed(fig.get_next_down_position()):
+                fig.move_down()
+            else:
+                board.add(fig)
+                board.compact()
+                fig = Shape(25)
+                _random_position = vec(random.randint(0, int(BOARD_SIZE.x-4)), 0)
+                if board.can_it_be_placed(fig.get_move_position(_random_position)):
+                    fig.move(_random_position)
+                else:
+                    gameover = True
+                    continue
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit()
+                    if event.key == pygame.K_LEFT:
+                        if board.can_it_be_placed(fig.get_next_left_position()):
+                            fig.move_left()
+                    if event.key == pygame.K_RIGHT:
+                        if board.can_it_be_placed(fig.get_next_right_position()):
+                            fig.move_right()
+                    if event.key == pygame.K_DOWN:
+                        FPS += 15
+                    if event.key == pygame.K_SPACE:
+                        if board.can_it_be_placed(fig.get_rotated_position()):
+                            fig.rotate_clockwise()
+                    if event.key == pygame.K_n:
+                        gameover = False
+                        board = Board(BOARD_SIZE)
+                        fig = Shape(25)
+                        random_x = random.randint(0, WIDTH // 25)
+                        while not board.can_it_be_placed(fig.get_move_position(vec(random_x, 0))):
+                            random_x = random.randint(0, WIDTH // 25)
+                        fig.move(vec(random_x, 0))
+                        continue
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_DOWN:
+                        FPS = 5
+            displaysurface.fill((0, 0, 0))
+            for y in range(1, HEIGHT // 25):
+                pygame.draw.line(displaysurface, (254, 0, 0),  (0, y * 25), (WIDTH, y * 25))
+            for x in range(1, WIDTH // 25):
+                pygame.draw.line(displaysurface, (254, 0, 0), (x*25, 0), (x*25, HEIGHT))
+            fig.draw(displaysurface)
+        else:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit()
+                    if event.key == pygame.K_n:
+                        gameover = False
+                        board = Board(BOARD_SIZE)
+                        fig = Shape(25)
+                        random_x = random.randint(0, WIDTH // 25)
+                        while not board.can_it_be_placed(fig.get_move_position(vec(random_x, 0))):
+                            random_x = random.randint(0, WIDTH // 25)
+                        fig.move(vec(random_x, 0))
+                        continue
+            displaysurface.fill((0, 0, 0))
+            for y in range(1, HEIGHT // 25):
+                pygame.draw.line(displaysurface, (254, 0, 0),  (0, y * 25), (WIDTH, y * 25))
+            for x in range(1, WIDTH // 25):
+                pygame.draw.line(displaysurface, (254, 0, 0), (x*25, 0), (x*25, HEIGHT))
+            fig.draw(displaysurface)
+            board.draw(displaysurface)
+            text_surface = myfont.render('Game Over!', False, (255, 0, 0), (0, 0, 0, 255))
+            text_surface.get_rect(topleft=(10 * 25, 16 * 25))
+            displaysurface.blit(text_surface, text_surface.get_rect(topleft=(5 * 25, 16 * 25)))
+        board.draw(displaysurface)
+        pygame.display.update()
+        FramePerSec.tick(FPS)
---
+++ shape.py
@@ -0,0 +1,152 @@
+import random
+import pygame
+from pygame.math import Vector2 as vec
+
+
+class Primitive(pygame.sprite.Sprite):
+    def __init__(self, pos, size):
+        super().__init__()
+        self.image = pygame.Surface((size, size))
+        self.position = pos
+        self.size = size
+        self.rect = self.image.get_rect(topleft=(self.position.x * self.size, self.position.y * self.size))
+        self.image.fill((255, 255, 0))
+
+    def move(self, new_pos):
+        self.rect = self.image.get_rect(topleft=(new_pos.x * self.size, new_pos.y * self.size))
+        self.position = new_pos
+
+    def move_left(self):
+        self.position += vec(-1, 0)
+        self.rect = self.image.get_rect(topleft=(self.position.x * self.size, self.position.y * self.size))
+
+    def move_right(self):
+        self.position += vec(1, 0)
+        self.rect = self.image.get_rect(topleft=(self.position.x * self.size, self.position.y * self.size))
+
+    def move_down(self):
+        self.position += vec(0, 1)
+        self.rect = self.image.get_rect(topleft=(self.position.x * self.size, self.position.y * self.size))
+
+    def rotate_clockwise(self, center=vec(0, 0)):
+        normal_vector = self.position - center
+        normal_vector.x, normal_vector.y = normal_vector.y, -normal_vector.x
+        self.position = normal_vector + center
+        self.rect = self.image.get_rect(topleft=(self.position.x * self.size, self.position.y * self.size))
+
+    def get_rotated_position(self, center=vec(0, 0)):
+        normal_vector = self.position - center
+        normal_vector.x, normal_vector.y = normal_vector.y, -normal_vector.x
+        return normal_vector + center
+
+    def get_position(self):
+        return self.position
+
+
+class Shape(pygame.sprite.Group):
+    shapes = {
+        1: ((0, 0), (1, 0), (2, 0), (3, 0)),        # line
+        2: ((0, 0), (1, 0), (2, 0), (0, 1)),        # left angle
+        3: ((0, 0), (1, 0), (2, 0), (2, 1)),        # right angle
+        4: ((0, 0), (1, 0), (0, 1), (1, 1)),        # cube
+        5: ((0, 0), (1, 0), (2, 0), (1, 1)),        # T-shape
+        6: ((0, 0), (1, 0), (1, 1), (2, 1)),        # left Z
+        7: ((0, 1), (1, 1), (1, 0), (2, 0))         # right Z
+    }
+
+    def __init__(self, scaler):
+        super().__init__()
+        for pos in self.shapes[random.randint(1, 7)]:
+            self.add(Primitive(vec(pos), scaler))
+
+    def get_move_position(self, pos):
+        vects = list()
+        for element in self:
+            vects.append(element.get_position() + pos)
+        return vects
+
+    def move(self, pos):
+        for element in self:
+            new_position = element.position + pos
+            element.move(new_position)
+        return True
+
+    def get_position(self):
+        vects = list()
+        for element in self:
+            vects.append(element.get_position())
+        return vects
+
+    def get_next_left_position(self):
+        shape = list()
+        for element in self:
+            shape.append(element.get_position() + vec(-1, 0))
+        return shape
+
+    def move_left(self):
+        for element in self:
+            element.move_left()
+
+    def get_next_right_position(self):
+        shape = list()
+        for element in self:
+            shape.append(element.get_position() + vec(1, 0))
+        return shape
+
+    def move_right(self):
+        for element in self:
+            element.move_right()
+        return True
+
+    def get_next_down_position(self):
+        shape = list()
+        for element in self:
+            shape.append(element.get_position() + vec(0, 1))
+        return shape
+
+    def move_down(self):
+        for element in self:
+            element.move_down()
+
+    def get_rotated_position(self):
+        shape = list()
+        center = self.sprites()[1].position
+        for element in self:
+            shape.append(element.get_rotated_position(center))
+        return shape
+
+    def rotate_clockwise(self):
+        center = self.sprites()[1].position
+        for element in self:
+            element.rotate_clockwise(center)
+
+
+class Board(pygame.sprite.Group):
+    def __init__(self, board_size: vec):
+        super().__init__()
+        self.board_size = board_size
+
+    def can_it_be_placed(self, shape_vects: list):
+        for el in shape_vects:
+            if el.x < 0 or el.y < 0 or el.x >= self.board_size.x or el.y >= self.board_size.y:
+                return False
+        for element in self:
+            if element.get_position() in shape_vects:
+                return False
+        return True
+
+    def compact(self):
+        line = self.board_size.y
+        while line >= 0:
+            line_primitives = pygame.sprite.Group()
+            for element in self:
+                if element.get_position().y == line:
+                    line_primitives.add(element)
+            if len(line_primitives) == int(self.board_size.x):
+                print(f"Line num {line}, Elements in line: {len(line_primitives)}, line size: {self.board_size.x}")
+                self.remove(line_primitives)
+                for el in self:
+                    if el.get_position().y < line:
+                        el.move_down()
+                line += 1
+            line -= 1
